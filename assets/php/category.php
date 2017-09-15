d55c246c2158519413a39dad7b4562446f9980af
wasuradananjith <wasuradananjith@gmail.com>
2017-09-15 23:04:41 +0530

    text aligned center category

--- assets/php/category.php
+++ assets/php/category.php
@@ -9,17 +9,22 @@
 echo "                    <div class=\"categories col-sm-2\" id=\"sticky\">
                              
                          	<div class=\"col-xs-12\" id=\"sticky-anchor\">
+                         	    <h5>Transport</h5>
                          		<img src=\"images/transport_logo.jpg\" class=\"category-items\">
                          	</div>
                          	<div class=\"col-xs-12 \">
+                         	    <h5>Technology</h5>
                                 <img src=\"images/technology_logo.jpg\" class=\"category-items\">
                          	</div>
                          	<div class=\" col-xs-12 \">
+                         	    <h5>Education</h5>
                                 <img src=\"images/education_logo.jpg\" class=\"category-items\">
                          	</div>
                          	<div class=\"col-xs-12 \">
+                         	    <h5>Social</h5>
                                 <img src=\"images/social_logo.jpg\" class=\"category-items\">
                          	</div>
                          	<div class=\"col-xs-12 \">
+                         	    <h5>Other</h5>
                                 <img src=\"images/other_logo.jpg\" class=\"category-items\">
                         </div>";
